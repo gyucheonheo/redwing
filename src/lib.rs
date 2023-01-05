7386a20cbb219b63473358e76d0305f98975ede1
gheo <gyucheon.heo@ascension.org>
2023-01-04 23:02:37 -0600

    make idea work

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+
 pub struct Config {
     pub path: String,
     pub cmd: Vec<String>
@@ -5,11 +6,12 @@ pub struct Config {
 
 impl Config {
     pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments!");
-        }
+//        if args.len() < 3 {
+//            return Err("Not enough arguments!");
+//        }
         let path = args[1].clone();
-        let cmd:Vec<String> = (&args[2..]).to_vec();
+        let cmd: Vec<String> = (&args[2..])
+            .to_vec();
 
         Ok(Config { path, cmd })
     }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
-use std::env;
-use fork::{daemon, Fork};
-use std::process::Command;
+use std::{
+    env,
+    process::Command
+};
 
 use redwing::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
     let _config = Config::build(&args).unwrap();
+    let program = &_config.cmd[0];
+    let rest = &_config.cmd[1..];
+    let mut _cmd = Command::new(program);
 
-    if let Ok(Fork::Child) = daemon(false, false) {
-       Command::new("sleep")
-           .arg("300")
-           .output()
-           .expect("failed to execute process");
+    _cmd.current_dir(_config.path);
+
+    if rest.len() == 0 {
+        _cmd.status().expect("process failed to execute");
+    } else {
+        _cmd.args(rest).status().expect("process failed to execute");
     }
 }
