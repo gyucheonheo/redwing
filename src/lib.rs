014e5a8d88c6a5fe90da02a426b61d483758a45c
gheo <gyucheon.heo@ascension.org>
2023-01-10 21:22:31 -0600

    Refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-
+use std::process::Command;
 pub struct Config {
     pub path: String,
     pub cmd: Vec<String>
@@ -6,13 +6,28 @@ pub struct Config {
 
 impl Config {
     pub fn build(args: &[String]) -> Result<Config, &'static str> {
-//        if args.len() < 3 {
-//            return Err("Not enough arguments!");
-//        }
+        if args.len() < 3 {
+            return Err("Not enough arguments!");
+        }
         let path = args[1].clone();
         let cmd: Vec<String> = (&args[2..])
             .to_vec();
 
         Ok(Config { path, cmd })
     }
+}
+
+pub fn run(config: Config) -> Result<std::process::ExitStatus, std::io::Error>{
+    let program = &config.cmd[0];
+    let rest = &config.cmd[1..];
+    let mut binding = Command::new(program);
+    
+    binding.current_dir(config.path);
+
+    let status = match rest {
+        [] => binding.status(),
+        otherwise => binding.args(otherwise).status(),
+    };
+
+    status
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,17 @@
-use std::{
-    env,
-    process::Command
-};
+use std::env;
 
 use redwing::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let _config = Config::build(&args).unwrap();
-    let program = &_config.cmd[0];
-    let rest = &_config.cmd[1..];
-    let mut _cmd = Command::new(program);
 
-    _cmd.current_dir(_config.path);
+    let _config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Error: {}", err);
+        std::process::exit(1);
+    });
 
-    if rest.len() == 0 {
-        _cmd.status().expect("process failed to execute");
-    } else {
-        _cmd.args(rest).status().expect("process failed to execute");
-    }
+    redwing::run(_config).unwrap_or_else(|err| {
+        eprintln!("reding failed to run: {}", err);
+        std::process::exit(1);
+    });
 }
